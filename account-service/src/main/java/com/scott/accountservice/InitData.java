71117f3a9ae18b77f3f7959e2c7e89839ccc3006
lap-top <s3716716@student.rmit.edu.au>
2022-10-11 03:22:50 +1100

    Populate account types on database load

---
+++ account-service/src/main/java/com/scott/accountservice/InitData.java
@@ -0,0 +1,27 @@
+package com.scott.accountservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+// For debuging
+@Component
+public class InitData implements ApplicationListener<ContextRefreshedEvent> {
+    private boolean setup = false;
+    @Autowired
+    private JdbcTemplate jdbcTmpl;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (setup) return;
+
+
+        // Create Account Types
+        jdbcTmpl.execute("INSERT INTO account_type VALUES(1, 'Loan')");
+        jdbcTmpl.execute("INSERT INTO account_type VALUES(2, 'Savings')");
+        jdbcTmpl.execute("INSERT INTO account_type VALUES(3, 'Term Investment')");
+
+    }
+}
