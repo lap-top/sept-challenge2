4344835d794978075d3965454458348b0e5aa20c
lap-top <s3716716@student.rmit.edu.au>
2022-10-10 23:33:06 +1100

    Added Validation exception

--- user-service/src/main/java/com/scott/userservice/exception/RestExceptionHandler.java
+++ user-service/src/main/java/com/scott/userservice/exception/RestExceptionHandler.java
@@ -4,10 +4,12 @@ package com.scott.userservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -16,6 +18,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(value = {RequestException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> handleRequestException(RequestException e) {
         RestException exception = new RestException(
                 e.getMessage(),
@@ -27,9 +30,22 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(value = {NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleNotFoundException(NotFoundException e) {
         RestException exception = new RestException(
                 e.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now(ZoneId.of("Z")),
+                e
+        );
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationException(ValidationException e) {
+        RestException exception = new RestException(
+                "["+e.violations.stream().map(ex -> ex.getMessage().toString()).collect(Collectors.joining(", "))+"]",
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")),
                 e
