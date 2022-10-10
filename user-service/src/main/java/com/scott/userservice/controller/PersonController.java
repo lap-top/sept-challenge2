fbc05570603eef8b474f2659eeaf893fe5966ba0
lap-top <s3716716@student.rmit.edu.au>
2022-10-10 23:33:38 +1100

    Added PUT with successful validation

--- user-service/src/main/java/com/scott/userservice/controller/PersonController.java
+++ user-service/src/main/java/com/scott/userservice/controller/PersonController.java
@@ -3,15 +3,22 @@ package com.scott.userservice.controller;
 import com.scott.userservice.dao.PersonRepository;
 import com.scott.userservice.exception.NotFoundException;
 import com.scott.userservice.exception.RequestException;
+import com.scott.userservice.exception.ValidationException;
 import com.scott.userservice.model.Person;
+import com.scott.userservice.utils.RestUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
+import javax.validation.Validator;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @RestController
 @Validated
@@ -19,6 +26,8 @@ public class PersonController {
     @Autowired
     PersonRepository personRepository;
 
+    @Autowired
+    private Validator validator;
     @GetMapping("/person/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable("id") @Min(value =0) long id) {
         return new ResponseEntity<>(personRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found")), HttpStatus.OK);
@@ -29,7 +38,20 @@ public class PersonController {
         if (personRepository.findByEmail(person.getEmail()) != null)
             throw new RequestException("Email Already Exist");
 
-        Person newPerson = personRepository.save(person);
-        return new ResponseEntity<>(newPerson, HttpStatus.OK);
+        return new ResponseEntity<>(personRepository.save(person), HttpStatus.OK);
+    }
+
+    @PutMapping("/persons/person")
+    public ResponseEntity<Person> updatePerson(@NotNull @RequestBody Person person) throws RequestException, ValidationException {
+        if (person.getId() == null)
+            throw new RequestException("User id must be in request body");
+        BeanUtils.copyProperties(person, existingPerson, RestUtils.nullProperties(person));
+        Set<ConstraintViolation<Person>> violations = validator.validate(existingPerson);
+
+        if (violations.size() > 0){
+            throw new ValidationException(violations);
+        }
+
+        return new ResponseEntity<>(personRepository.save(existingPerson), HttpStatus.OK);
     }
 }
