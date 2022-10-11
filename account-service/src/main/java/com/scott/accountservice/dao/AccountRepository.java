2141fbc60f07d33e803d0ea13cfc410e2c1028d6
lap-top <s3716716@student.rmit.edu.au>
2022-10-11 16:04:12 +1100

    Created inter-microservice queries.

--- account-service/src/main/java/com/scott/accountservice/dao/AccountRepository.java
+++ account-service/src/main/java/com/scott/accountservice/dao/AccountRepository.java
@@ -2,7 +2,22 @@ package com.scott.accountservice.dao;
 
 import com.scott.accountservice.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+
+@Transactional
 public interface AccountRepository extends JpaRepository<Account, Long>  {
+    
+    @Query(value="select case when exists(select * from person where id = :userId) then 'true' else 'false' end from dual", nativeQuery = true)
+        Boolean userExist(@Param("userId") Long searchId);
 
+    @Query(value="select name FROM person WHERE person.id = :userId", nativeQuery = true)
+    String getAccountName(@Param("userId") Long userId);
+
+
+    Optional<Account> findAccountByAccountNumber(long accountNumber);
 }
