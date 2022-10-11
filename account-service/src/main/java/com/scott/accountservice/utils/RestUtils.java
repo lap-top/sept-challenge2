0157c2a4e1ba4a86890cd27e7ebc4dcd33265d2e
lap-top <s3716716@student.rmit.edu.au>
2022-10-11 16:02:27 +1100

    Added util function for PUT mapping

---
+++ account-service/src/main/java/com/scott/accountservice/utils/RestUtils.java
@@ -0,0 +1,14 @@
+package com.scott.accountservice.utils;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.Arrays;
+
+public class RestUtils {
+    public static String[] nullProperties(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        return Arrays.stream(pds).filter(pd -> src.getPropertyValue(pd.getName()) == null).map(pd -> pd.getName()).toArray(value -> new String[value]);
+    }
+}
