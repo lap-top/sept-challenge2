4849a4a45c990778c972cea366bf636724246fd6
lap-top <s3716716@student.rmit.edu.au>
2022-10-11 17:13:05 +1100

    Added validation parameters

--- account-service/src/main/java/com/scott/accountservice/model/Account.java
+++ account-service/src/main/java/com/scott/accountservice/model/Account.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Entity
 @Getter
@@ -20,12 +23,15 @@ public class Account {
     private Long accountNumber;
 
     @JoinColumn(name = "person_id")
+    @NotNull(message="Must specify 'id' (account owner)")
+    @Positive(message="Must be positive long/int")
     private Long id;
 
     private String accountName;
 
     @ManyToOne
     @JoinColumn(name="accountType_type", nullable=false)
+    @NotNull(message="accountType must not be null")
     private AccountType accountType;
 
     private double balance = 0;
