f1af1ab9fc1eacf4309552702de6145cd9ac880c
lap-top <s3716716@student.rmit.edu.au>
2022-10-11 17:57:10 +1100

    Updated validation return messages

--- user-service/src/main/java/com/scott/userservice/model/Person.java
+++ user-service/src/main/java/com/scott/userservice/model/Person.java
@@ -19,17 +19,17 @@ public class Person {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    @NotBlank(message = "Name must not be blank")
+    @NotBlank(message = "name must not be blank")
     private String name;
-    @NotBlank  private String address;
-    @Size(min=4, max=4, message= "Postcodes must be 4 digits")
+    @NotBlank(message="address must not be blank")  private String address;
+    @Size(min=4, max=4, message= "postcode must be 4 digits")
     private String postcode;
-    @NotNull @Min(value =0, message="Age must be at least 0") @Max(value=200, message="Age is too high")
+    @NotNull(message = "age must not be null") @Min(value =0, message="age must be at least 0") @Max(value=200, message="age is too high")
     int age;
-    @NotBlank private
+    @NotBlank(message="job must not be blank") private
     String job;
-    @NotBlank @Email(message = "Email should be valid format")
+    @NotBlank(message="email must not be blank") @Email(message = "email should be valid format")
     private String email;
-    @NotBlank @Size (min = 10, max = 10, message = "Phone numbers must have length of 10")
+    @NotBlank(message="phoneno must not be blank") @Size (min = 10, max = 10, message = "Phone numbers must have length of 10")
     private String phoneno;
 }
